47d541c7200ac4b1632a54527e74eb743e6a9ee6
Tomas <tomasysh@gmail.com>
2022-07-24 21:21:43 +0800

    4. SEO with vue-meta

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,7 +11,18 @@ import NavBar from '~/components/Navbar.vue';
 export default {
     components: {
         NavBar
+    },
+    head() {
+        return {
+            titleTemplate: '%s - Real World Events',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'Where you can find all the events taking place in your neighborhood'
+                }
+            ]
+        }
     }
 }
-
 </script>
