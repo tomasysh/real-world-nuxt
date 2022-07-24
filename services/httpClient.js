e1431bdd513364f8d0ee217b9264d455bc375bca
Tomas <tomasysh@gmail.com>
2022-07-24 23:22:24 +0800

    pre- heroku

--- services/httpClient.js
+++ services/httpClient.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const http = axios.create({
-    baseURL: `http://localhost:3000`,
+    // baseURL: `http://localhost:3000`,
+    baseURL: `https://my-json-server.typicode.com/tomasysh/real-world-nuxt`,
     withCredentials: false,
     headers: {
         Accept: 'application/json',
