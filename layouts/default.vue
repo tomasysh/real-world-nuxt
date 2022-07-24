b1037be9916c87d638eb045e2f0e0134af63570a
Tomas <tomasysh@gmail.com>
2022-07-24 21:03:34 +0800

    2. Creating a Nuxt App

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">
+        <NavBar />
+        <nuxt />
+    </div>
+</template>
+
+<script>
+import NavBar from '~/components/Navbar.vue';
+
+export default {
+    components: {
+        NavBar
+    }
+}
+
+</script>
